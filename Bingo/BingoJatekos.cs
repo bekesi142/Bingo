913a91b6c54daa0c2ea6527b9be10778e379106d
bekesi142 <72067150+bekesi142@users.noreply.github.com>
2024-06-05 10:08:02 +0200

    Add files via upload

---
+++ Bingo/BingoJatekos.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bingo
+{
+    internal class BingoJatekos
+    {
+        string szam1, szam2, szam3, szam4, szam5;
+
+        public BingoJatekos(string mezo) {
+            var sor = mezo.Split(";");
+            szam1 = sor[0];
+            szam2 = sor[1];
+            szam3 = sor[2];
+            szam4 = sor[3];
+            szam5 = sor[4];
+
+        
+        }
+
+        public BingoJatekos(string szam1, string szam2, string szam3, string szam4, string szam5)
+        {
+            this.Szam1 = szam1;
+            this.Szam2 = szam2;
+            this.Szam3 = szam3;
+            this.Szam4 = szam4;
+            this.Szam5 = szam5;
+        }
+
+        public string Szam1 { get => szam1; set => szam1 = value; }
+        public string Szam2 { get => szam2; set => szam2 = value; }
+        public string Szam3 { get => szam3; set => szam3 = value; }
+        public string Szam4 { get => szam4; set => szam4 = value; }
+        public string Szam5 { get => szam5; set => szam5 = value; }
+    }
+
+    
+}
---
+++ Bingo/Program.cs
@@ -0,0 +1,61 @@
+﻿
+
+using System.IO;
+using System.Linq;
+using Bingo;
+
+
+
+internal class Program
+{
+    static List<BingoJatekos> lista = new List<BingoJatekos>();
+    static List<string> randomSzamok = new List<string>();
+    public static void Main(string[] args)
+    {
+        
+        foreach (var item in File.ReadAllLines("Andi.txt"))
+        {
+           
+            lista.Add(new BingoJatekos(item));
+
+
+        }
+
+        List<string> jatekosok = new List<string>();
+        foreach (string x in File.ReadAllLines("nevek.text"))
+        {
+            jatekosok.Add(x);
+        }
+
+        Console.WriteLine("4. feladat: Játékosok száma: " + jatekosok.Count);
+
+        
+        Random random = new Random();
+        int random1 = random.Next(1, 101);
+        int random2 = random.Next(1, 101);
+        int random3 = random.Next(1, 101);
+        int random4 = random.Next(1, 101);
+        int random5 = random.Next(1, 101);
+
+        randomSzamok.Add(Convert.ToString(random1));
+        randomSzamok.Add(Convert.ToString(random2));
+        randomSzamok.Add(Convert.ToString(random3));
+        randomSzamok.Add(Convert.ToString(random4));
+        randomSzamok.Add(Convert.ToString(random5));
+
+        
+    }
+
+    public bool BingoEll()
+    {
+        foreach (var szam in lista)
+        {
+            if (randomSzamok.Contains(szam))
+            {
+
+            }
+        }
+
+        return false;
+    }
+}
